cd3703bdb460770cf7f19b61587e011c27e7c3ca
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-09-20 19:27:01 -0300

    menuMobile

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,36 +1,65 @@
+"use client";
 import styles from "./styles.module.css";
 import Image from "next/image";
-import logo from "../../../../public/images/logo.png"
-//importar caminho da minha logo
+import logo from "../../../../public/images/logo.png";
 import Link from "next/link";
+import { MenuIcon } from "../Icons/iconsHome/iconMenu";
+import { useState, useEffect } from "react";
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", updateWindowWidth);
+
+    updateWindowWidth();
+
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-    
-      <div className={styles.logoContainer}>
-          <a>
-            <Image className={styles.img} src={logo} alt="Music For All Logo" />
+        <div className={styles.logoContainer}>
+          <a href="/">
+            <Image
+              className={styles.img}
+              src={logo}
+              alt="Logotipo Music For All"
+            />
           </a>
         </div>
 
-        <nav className={styles.navContainer}>
-          <Link href="/" className={styles.aContainer}>
-            Início
-          </Link>
+        <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
+          <MenuIcon />
+        </div>
 
+        {(menuOpen || windowWidth >= 768) && (
+          <nav className={styles.navContainer}>
+            <Link href="/" className={styles.aContainer}>
+              Início
+            </Link>
 
-          <Link href="/cursos" className={styles.aContainer}>
-            Cursos
-          </Link>
+            <Link href="/cursos" className={styles.aContainer}>
+              Cursos
+            </Link>
 
-          <Link href="/sobre" className={styles.aContainer}>
-            Sobre
-          </Link>
-
-  
-        </nav>
+            <Link href="/sobre" className={styles.aContainer}>
+              Sobre
+            </Link>
+          </nav>
+        )}
       </div>
     </header>
   );
