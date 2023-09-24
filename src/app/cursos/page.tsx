2724fd5dc4350a03eda12dc35823ba59296cbae0
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-09-24 20:18:07 -0300

    cursos

---
+++ src/app/cursos/page.tsx
@@ -0,0 +1,153 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+import Link from "next/link";
+import homeoficial from "../../../public/images/homeoficial.png"
+import homecursos from "../../../public/images/homecursos.png"
+import cursoviolao1 from "../../../public/images/cursoviolao1.png";
+import piano from "../../../public/images/piano.png";
+import guitarra from "../../../public/images/guitarra.png";
+import gaita from "../../../public/images/gaita.png";
+import ukulele from "../../../public/images/ukulele.png";
+import titlecursos2 from "../../../public/images/titlecursos2.png";
+import agendaraula from "../../../public/images/agendaraula.png";
+export default function Cursos() {
+  return (
+    <>
+      <main>
+        <div className={styles.containerViolao}>
+               
+      <Image
+                className={styles.img}
+                src={homeoficial}
+                alt="Music For All Logo" 
+                width={1895} 
+                height={598}
+                quality={100}
+              />
+
+
+          <Image
+            className={styles.violaoContainer}
+            src={homecursos}
+            alt="Music For All Logo"
+          />
+        </div>
+
+       
+
+        <div className={styles.containerTwo}>
+          <div className={styles.divTitle}>
+            <Image
+              className={styles.titleCursos}
+              src={titlecursos2}
+              alt="Titulo  da sessão dois"
+        
+            />
+
+            <span className={styles.spanCursos}>
+              {" "}
+              ESCOLHA O QUE MAIS LHE INSPIRA!
+            </span>
+          </div>
+         
+         <div className={styles.titlesection}> <span > Nossos cursos de música são uma sinfonia de excelência e aprendizado. </span></div>
+
+          <section className={styles.cursosSection}>
+            
+            
+            <div className={styles.curso}>
+              <Link href={"/cursos/violao"}>
+                <Image
+                  className={styles.imgCursos}
+                  src={cursoviolao1}
+                  alt="Imagem do curso"
+                
+                />
+              </Link>
+            </div>
+
+            <div className={styles.curso}>
+              <Link href={"/cursos/piano"}>
+                <Image
+                  className={styles.imgCursos}
+                  src={piano}
+                  alt="Imagem do curso"
+                 
+                />
+              </Link>
+            </div>
+
+            <div className={styles.curso}>
+              <Link href={"/cursos/guitarra"}>
+                <Image
+                  className={styles.imgCursos}
+                  src={guitarra}
+                  alt="Imagem do curso"
+                  
+                />
+              </Link>
+            </div>
+
+            <div className={styles.curso}>
+              <Link href={"/cursos/gaita"}>
+                <Image
+                  className={styles.imgCursos}
+                  src={gaita}
+                  alt="Imagem do curso"
+                  
+                />
+              </Link>
+            </div>
+
+            <div className={styles.curso}>
+              <Link href={"/cursos/ukulele"}>
+                <Image
+                  className={styles.imgCursos}
+                  src={ukulele}
+                  alt="Imagem do curso"
+                
+                />
+              </Link>
+            </div>
+          </section>
+         
+        </div>
+
+        
+          <div className={styles.agendar}>
+            <button className={styles.buttonPresencial}>Presencial</button>
+
+            <button className={styles.buttonOnline}>Online</button>
+            <Image
+              className={styles.agendaraula}
+              src={agendaraula}
+              alt="Music For All Logo"
+            
+            
+            />
+          </div>
+
+
+
+          <div className={styles.formContainer}>
+            <h2 className={styles.formTitle}>
+              Gostaria de mais informações? Preencha o formulário abaixo!
+            </h2>
+            <form>
+              <label  className={styles.formLabel} htmlFor="nome">Nome:</label>
+              <input className={styles.formInput} type="text" id="nome" name="nome" />
+
+              <label  className={styles.formLabel} htmlFor="email">Email:</label>
+              <input className={styles.formInput} type="email" id="email" name="email" />
+
+              <label  className={styles.formLabel} htmlFor="whatsapp">WhatsApp:</label>
+              <input className={styles.formInput} type="text" id="whatsapp" name="whatsapp" />
+
+              <button className={styles.formButton} type="submit">Quero mais informações!</button>
+            </form>
+          </div>
+        
+      </main>
+    </>
+  );
+}
