9cd93ec50235e6a151d6c85f6e156d4aabb3499a
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-09-25 15:29:38 -0300

    Update page.tsx

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -128,8 +128,8 @@ export default function Cursos() {
             />
           </div>
 
-<div className={styles.formCursos}></div>
-<Form/>
+<div className={styles.formCursos}><Form/></div>
+
         
       </main>
     </>
