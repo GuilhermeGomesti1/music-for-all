78c7614944775064dcbbf078d76027cd9378c382
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-10-07 03:14:31 -0300

    Update page.tsx

--- src/app/alunos/page.tsx
+++ src/app/alunos/page.tsx
@@ -15,8 +15,28 @@ import { auth } from "../firebase";
 import Link from "next/link";
 import styles from "./styles.module.css";
 
+
 export default function Alunos() {
+  const [loading, setLoading] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+
   useEffect(() => {
+    setTimeout(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+          activateScrollAnimations(); // Ativar animações quando o usuário estiver logado
+        } else {
+          setLoggedIn(false);
+        }
+        setLoading(false);
+      });
+
+      return () => unsubscribe();
+    }, 2000); // Tempo de carregamento simulado de 2 segundos (você pode ajustar isso)
+  }, []);
+
+  function activateScrollAnimations() {
     if (process.browser) {
       import("scrollreveal").then((ScrollRevealModule) => {
         const ScrollReveal = ScrollRevealModule.default || ScrollRevealModule;
@@ -33,146 +53,132 @@ export default function Alunos() {
         });
       });
     }
-  }, []);
-
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // O usuário está autenticado, atualize a variável de estado
-        setLoggedIn(true);
-      } else {
-        // O usuário não está autenticado, mantenha a variável de estado como falsa
-        setLoggedIn(false);
-      }
-    });
-
-    // Certifique-se de cancelar a inscrição quando o componente for desmontado
-    return () => unsubscribe();
-  }, []);
+  }
 
   return (
     <div className={styles.main}>
-      {loggedIn ? (
-        // Conteúdo exclusivo para alunos autenticados
-        <>
-          <main className={styles.main}>
-            <div >
-              <Image
-                className={styles.facaparte}
-                src={facaparte}
-                alt="foto dos alunos"
-                width={1833}
-                height={911}
-                loading="lazy"
-                placeholder="blur"
-              />
-            </div>
-
-            <div className={styles.imgMobile}>
-              <Image
-                className={styles.imgMobile}
-                src={facapartemobile}
-                alt="foto home mobile"
-                width={390}
-                height={658}
-                loading="lazy"
-                placeholder="blur"
-              />
-            </div>
-
-            <section className={`${styles.alunosSection} animated-item`}>
-
-              
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={videoaulas}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={materiais}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={agenda}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={forum}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={pratica}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-
-              <div className={`${styles.conteudoalunos} animated-item`}>
-                <Link href={"/videoaulas"}>
-                  <Image
-                    className={styles.imgCursos}
-                    src={palcovirtual}
-                    alt="Imagem do curso"
-                    loading="lazy"
-                    placeholder="blur"
-                  />
-                </Link>
-              </div>
-              <IconWhatsappfixed />
-            </section>
-          </main>
-        </>
+      {loading ? (
+        <div className={styles.loading}>
+          Carregando...
+        </div>
       ) : (
-        // Mensagem de login e link para a página de login
-        <div>
-          <strong>Faça o login para ter acesso</strong>
-          <Link href="/dashboard" passHref>
-            <button className={styles.buttons}>
-              <span className={styles.iconButton}></span>
-              <span className={styles.titleButton}>Aluno</span>
-              <span className={styles.subtitleButton}>
-                Clique aqui para acessar o seu conteúdo exclusivo!
-              </span>
-            </button>
-          </Link>
+        <div className={styles.main}>
+          {loggedIn ? (
+            <>
+              <main className={styles.main}>
+                <div>
+                  <Image
+                    className={styles.facaparte}
+                    src={facaparte}
+                    alt="foto dos alunos"
+                    width={1833}
+                    height={911}
+                    loading="lazy"
+                    placeholder="blur"
+                  />
+                </div>
+
+                <div className={styles.imgMobile}>
+                  <Image
+                    className={styles.imgMobile}
+                    src={facapartemobile}
+                    alt="foto home mobile"
+                    width={390}
+                    height={658}
+                    loading="lazy"
+                    placeholder="blur"
+                  />
+                </div>
+
+                <section className={`${styles.alunosSection}`}>
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={videoaulas}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={materiais}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={agenda}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={forum}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={pratica}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+
+                  <div className={`${styles.conteudoalunos} animated-item`}>
+                    <Link href={"/videoaulas"}>
+                      <Image
+                        className={styles.imgCursos}
+                        src={palcovirtual}
+                        alt="Imagem do curso"
+                        loading="lazy"
+                        placeholder="blur"
+                      />
+                    </Link>
+                  </div>
+                </section>
+              </main>
+            </>
+          ) : (
+            <div>
+              <strong>Faça o login para ter acesso</strong>
+              <Link href="/dashboard" passHref>
+                <button className={styles.buttons}>
+                  <span className={styles.iconButton}></span>
+                  <span className={styles.titleButton}>Aluno</span>
+                  <span className={styles.subtitleButton}>
+                    Clique aqui para acessar o seu conteúdo exclusivo!
+                  </span>
+                </button>
+              </Link>
+            </div>
+          )}
         </div>
       )}
     </div>
