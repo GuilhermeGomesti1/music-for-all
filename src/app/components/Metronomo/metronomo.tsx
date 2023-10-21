aac61e10807a4d4a50337e9719f410c5cb11f388
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-10-21 07:47:44 -0300

    att

--- src/app/components/Metronomo/metronomo.tsx
+++ src/app/components/Metronomo/metronomo.tsx
@@ -1,6 +1,10 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react"; // Importe o useEffect
+
+import Timer from "../timer/timer";
+import React, { useEffect, useRef, useState } from "react";
+
 import styles from "./styles.module.css";
+
 export default function Metronome() {
   const [bpm, setBpm] = useState(140);
   const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
@@ -8,53 +12,39 @@ export default function Metronome() {
   const [currentBeat, setCurrentBeat] = useState(0);
   const tempoDisplayRef = useRef<HTMLElement | null>(null);
   const tempoSliderRef = useRef<HTMLInputElement | null>(null);
-  const subtractBeatsButtonRef = useRef<HTMLElement | null>(null);
-  const addBeatsButtonRef = useRef<HTMLElement | null>(null);
-  const measureCountRef = useRef<HTMLElement | null>(null);
+  const subtractBeatsButtonRef = useRef<HTMLButtonElement | null>(null);
+const addBeatsButtonRef = useRef<HTMLButtonElement | null>(null);
+const measureCountRef = useRef<HTMLDivElement | null>(null);
 
   const [audios, setAudios] = useState<{ click1: HTMLAudioElement | null, click2: HTMLAudioElement | null }>({
     click1: null,
     click2: null,
   });
-  let metronomeInterval: NodeJS.Timeout | null = null;
 
-  const handleStartStopClick = () => {
-    if (isPlaying) {
-      stopMetronome();
+  const timer = new Timer(() => {
+    let audio;
+    if (currentBeat % beatsPerMeasure === 0) {
+      audio = audios.click1;
     } else {
-      startMetronome();
+      audio = audios.click2;
     }
-  };
+
+    if (audio) {
+      audio.play();
+    }
+
+    setCurrentBeat((prevBeat) => (prevBeat + 1) % beatsPerMeasure);
+  }, 60000 / bpm, { immediate: true });
 
   const startMetronome = () => {
+    timer.start();
     setIsPlaying(true);
-    const interval = 60000 / bpm;
-    let beatCount = 0;
-  
-    metronomeInterval = setInterval(() => {
-      let audio;
-      if (beatCount % beatsPerMeasure === 0) {
-        audio = audios.click1;
-      } else {
-        audio = audios.click2;
-      }
-  
-      if (audio) {
-        audio.play();
-      }
-  
-      beatCount++;
-      setCurrentBeat((prevBeat) => (prevBeat + 1) % beatsPerMeasure);
-    }, interval);
   };
 
   const stopMetronome = () => {
-    if (metronomeInterval !== null) {
-      clearInterval(metronomeInterval);
-      metronomeInterval = null;
-      setIsPlaying(false);
-      setCurrentBeat(0);
-    }
+    timer.stop();
+    setIsPlaying(false);
+    setCurrentBeat(0);
   };
 
   useEffect(() => {
@@ -67,7 +57,6 @@ export default function Metronome() {
   }, []);
 
   useEffect(() => {
-   
     if (tempoDisplayRef.current && tempoSliderRef.current) {
       const updateMetronome = () => {
         if (tempoDisplayRef.current && tempoSliderRef.current) {
@@ -111,7 +100,7 @@ export default function Metronome() {
         if (measureCountRef.current) {
           measureCountRef.current.textContent = newBeats.toString();
         }
-        })
+      });
     }
 
     if (addBeatsButtonRef.current) {
@@ -139,31 +128,11 @@ export default function Metronome() {
     };
   }, [bpm]);
 
-  useEffect(() => {
-    const startStopButton = document.querySelector(".startStop") as HTMLElement | null;
-
-    const handleStartStopClick = () => {
-      if (isPlaying) {
-        stopMetronome();
-      } else {
-        startMetronome();
-      }
-    };
-
-    if (startStopButton) {
-      startStopButton.addEventListener("click", handleStartStopClick);
-
-      return () => {
-        startStopButton.removeEventListener("click", handleStartStopClick);
-      };
-    }
-  }, [isPlaying]);
-  
   return (
     <div className={styles.container}>
       <div className={styles.metronome}>
         <div className={styles.bpmDisplay}>
-          <span className={styles.tempo}>{bpm}</span>
+          <span className={styles.tempo} ref={tempoDisplayRef}>{bpm}</span>
           <span className={`${styles.bpm} ${styles.ralewayfont}`}>BPM</span>
         </div>
         <div className={`${styles.tempoText} ${styles.ralewayfont}`}>
@@ -183,10 +152,8 @@ export default function Metronome() {
             max="280"
             step="1"
             value={bpm}
-            onChange={(event) => {
-              const newBpm = parseInt(event.target.value);
-              setBpm(newBpm);
-            }}
+            onChange={(event) => setBpm(parseInt(event.target.value))} 
+            ref={tempoSliderRef}
           ></input>
           <button
             className={`${styles.adjustTempobtn} ${styles.increaseTempo}`}
@@ -195,8 +162,13 @@ export default function Metronome() {
             +
           </button>
         </div>
-        <div className={`${styles.startStop} ${styles.ralewayfont}`} onClick={handleStartStopClick}>
-          {isPlaying ? "Parar" : "Iniciar"}
+        <div className={`${styles.startStop} ${styles.ralewayfont}`}>
+          <button onClick={startMetronome} disabled={isPlaying}>
+            Iniciar
+          </button>
+          <button className={styles.stopbutton} onClick={stopMetronome} disabled={!isPlaying}>
+            Parar
+          </button>
         </div>
         <div className={styles.measures}>
           <button
@@ -205,16 +177,20 @@ export default function Metronome() {
               const newBeats = Math.max(beatsPerMeasure - 1, 2);
               setBeatsPerMeasure(newBeats);
             }}
+            ref={subtractBeatsButtonRef}
           >
             -
           </button>
-          <div className={styles.measureCount}>{beatsPerMeasure}</div>
+          <div className={styles.measureCount} ref={measureCountRef}>
+            {beatsPerMeasure}
+          </div>
           <button
             className={`${styles.addBeats} ${styles.stepper}`}
             onClick={() => {
               const newBeats = Math.min(beatsPerMeasure + 1, 12);
               setBeatsPerMeasure(newBeats);
             }}
+            ref={addBeatsButtonRef}
           >
             +
           </button>
