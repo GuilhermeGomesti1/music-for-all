3adb6fb263d838dd9a7927490e5a23ba90a241f1
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-10-24 22:01:19 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */ 
-
 const withImages = require("next-images");
 
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  assetPrefix: "./", // Defina o caminho para arquivos estáticos
+  fileExtensions: ['mp3'], // Inclua a extensão MP3
+  esModule: false, // Configure esModule para ser false
 };
 
-module.exports = withImages({   fileExtensions: ['mp3'],
-  esModule: false, // Configure esModule to be false
+module.exports = withImages({
   ...nextConfig, // Mesclar com as configurações existentes
 });
