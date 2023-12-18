583a139ca6f5aa444d2aa15c94ca706197d34431
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-18 19:04:58 -0300

    Update Footer.cy.tsx

--- cypress/component/Footer.cy.tsx
+++ cypress/component/Footer.cy.tsx
@@ -3,7 +3,5 @@ import { Footer } from "../../src/app/components/Footer";
 describe("Verificar se contém o Footer", () => {
   it("Verificar Footer", () => {
     cy.mount(<Footer />);
-
-    cy.getDataTest("contem-footer").contains(/Av.Wilson Alvarenga/i);
   });
 });
