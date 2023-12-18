11a48cc07fdcf3879d6102e7aafec573c9b74763
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-18 04:56:02 -0300

    Update Form.cy.js

--- cypress/component/Form.cy.js
+++ cypress/component/Form.cy.js
@@ -6,14 +6,37 @@ describe("Verificar se o form está sendo preenchido e enviado", () => {
       /gostaria de mais informações/i
     );
     cy.getDataTest("form-contato-whatsapp").should("have.value", "");
+    cy.getDataTest("form-contato-whatsapp")
+      .find("#nome")
+      .should("have.value", "");
     cy.getDataTest("form-contato-whatsapp").find("#nome").type("Guilherme");
+    cy.getDataTest("form-contato-whatsapp")
+      .find("#nome")
+      .should("not.have.value", "");
+
+    cy.getDataTest("form-contato-whatsapp")
+      .find("#email")
+      .should("have.value", "");
     cy.getDataTest("form-contato-whatsapp")
       .find("#email")
       .type("guilherme@teste.com");
     cy.getDataTest("form-contato-whatsapp")
+      .find("#email")
+      .should("not.have.value", "");
+    cy.getDataTest("form-contato-whatsapp")
+      .find("#whatsapp")
+      .should("have.value", "");
+    cy.getDataTest("form-contato-whatsapp")
       .find("#whatsapp")
       .type("31986132070");
+    cy.getDataTest("form-contato-whatsapp")
+      .find("#whatsapp")
+      .should("not.have.value", "");
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
     cy.getDataTest("enviar-formulario").click();
+
     cy.getDataTest("form-contato-whatsapp").should("have.value", "");
   });
 });
