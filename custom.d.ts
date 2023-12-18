4149dc20562b7a6d302b36c571dee843ed75954e
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-18 20:26:57 -0300

    Update custom.d.ts

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,11 @@
-declare module '*.mp3' {
-    const value: string;
-    export default value;
+/// <reference types="cypress" />
+declare module "*.mp3" {
+  const value: string;
+  export default value;
+}
+
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    getDataTest(dataTestSelector: string): Chainable<JQuery<HTMLElement>>;
   }
+}
