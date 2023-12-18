d2f591de6ec135a9b5b2819f4261810182b84738
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-18 16:54:32 -0300

    Update Footer.cy.tsx

--- cypress/component/Footer.cy.tsx
+++
@@ -1,9 +0,0 @@
-import { Footer } from "../../src/app/components/Footer";
-
-describe("Verificar se contém o Footer", () => {
-  it("Verificar Footer", () => {
-    cy.mount(<Footer />);
-
-    cy.getDataTest("footer-contains").contains;
-  });
-});
