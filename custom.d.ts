a069c0e5c83e13cccf69343c59445a3b8782850a
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-18 20:30:13 -0300

    att

--- custom.d.ts
+++ custom.d.ts
@@ -1,11 +1,4 @@
-/// <reference types="cypress" />
 declare module "*.mp3" {
   const value: string;
   export default value;
-}
-
-declare namespace Cypress {
-  interface Chainable<Subject> {
-    getDataTest(dataTestSelector: string): Chainable<JQuery<HTMLElement>>;
-  }
 }
--- cypress/component/Footer.cy.tsx
+++ cypress/component/Footer.cy.tsx
@@ -2,8 +2,8 @@ import { Footer } from "../../src/app/components/Footer";
 import React from "react";
 describe("Verificar se contém o Footer", () => {
   it("Verificar Footer", () => {
-    cy.mount(<Footer />);
-
-    cy.getDataTest("contem-footer").contains(/Av.Wilson Alvarenga/i);
+    cy.mount(<Footer />)
+      .getDataTest("contem-footer")
+      .contains(/Av.Wilson Alvarenga/i);
   });
 });
