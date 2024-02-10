6f05fc1bbe5ff6452a89be793e06564b9945b290
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2024-02-10 19:52:18 -0300

    att

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-export default NextAuth({
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+const handler = NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
 });
+
+export { handler as GET, handler as POST };
