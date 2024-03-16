033de48d98dab4150aeaeb00fa9091bff36daca3
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2024-03-16 13:32:19 -0300

    att

--- src/app/components/Products/index.tsx
+++ src/app/components/Products/index.tsx
@@ -120,6 +120,7 @@ const AnimatedProductItem = ({
     <div ref={productRef} className={`${styles.listaProducts} animated-item`}>
       <div>
         <Link
+          title="Clique para saber mais"
           href={`/product/${product._id}?product=${JSON.stringify(product)}`}
         >
           <div className={styles.imageContainer}>
@@ -191,6 +192,7 @@ const AnimatedProductItem = ({
 
       <div className={styles.divtextCategory}>
         <Link
+          title="Clique para saber mais"
           className={styles.goProduct}
           href={`/product/${product._id}?product=${JSON.stringify(product)}`}
         >
