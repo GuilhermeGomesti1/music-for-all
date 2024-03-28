941f1ca93e1b8bbc94769727ff54a5fd3f84ee01
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2024-03-28 16:16:27 -0300

    Update index.tsx

--- src/app/components/scrooltotop/index.tsx
+++ src/app/components/scrooltotop/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 
 export default function ScrollToTop() {
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, -220);
   }, []);
   return null;
 }
